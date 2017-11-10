84c0b25cb23ccb66db7337b02cbc2704622cf05c
jroyals2 <jamesroyals3@gmail.com>
2017-11-10 16:31:59 -0500

    built out the backend routes in the events controller for events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -6,19 +6,37 @@ class Api::EventsController < ApplicationController
     end
 
     def show
-
+        event_id = params[:id]
+        @event = Event.find_by_id(event_id)
+        render json: @event
     end
 
     def create
-
+        @event = Event.create!(event_params)
+        render json: @event
     end
 
     def update
-
+        event_id = params[:id]
+        @event = Event.find_by_id(event_id)
+        @event.update_attributes(event_params)
+        render json: @event
     end
 
     def destroy
+        event_id = params[:id]
+        @event = Event.find_by_id(event_id)
+        @event.destroy
+        render json: {
+            msg: 'Delete the event successfully'
+        }
 
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:event_name, :location, :date, :teams) 
     end
 
 end
