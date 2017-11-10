b82f5d03e29e932b012afd9ace80986277d3756a
jroyals2 <jamesroyals3@gmail.com>
2017-11-10 17:20:33 -0500

    added full backend controller routes for the tailgate events controller.  tested in postman

--- app/controllers/api/tailgate_events_controller.rb
+++ app/controllers/api/tailgate_events_controller.rb
@@ -1,23 +1,44 @@
 class Api::TailgateEventsController < ApplicationController
 
     def index
-        
+        @tailgate_events = TailgateEvent.all
+        render json: @tailgate_events
     end
 
     def show
-    
+        tailgate_events_id = params[:id]
+        @tailgate_event = TailgateEvent.find_by_id(tailgate_events_id)
+        render json: @tailgate_event
     end
         
     def create
-        
+        @user = current_user
+        @tailgate_event = @user.tailgate_events.create!(tailgate_event_params)
+        render json: @tailgate_event
     end
         
     def update
+        tailgate_events_id = params[:id]
+        @tailgate_event = TailgateEvent.find_by_id(tailgate_events_id)
+        @tailgate_event.update_attributes(tailgate_event_params)
+        render json: @tailgate_event
         
     end
         
     def destroy
+        tailgate_events_id = params[:id]
+        @tailgate_event = TailgateEvent.find_by_id(tailgate_events_id)
+        @tailgate_event.destroy
+        render json: {
+            msg: "Delete this event successfully"
+        }
                 
     end
 
+    private
+
+    def tailgate_event_params
+    
+        params.require(:tailgate_event).permit(:tailgate_name, :about, :cost, :is_eighteen, :event_id) 
+      end
 end
