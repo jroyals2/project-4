e82872c4a6d678fbc96962257eedb76475cde991
jroyals2 <jamesroyals3@gmail.com>
2017-11-17 12:32:36 -0500

    added validations

--- app/models/tailgate_event.rb
+++ app/models/tailgate_event.rb
@@ -1,5 +1,9 @@
 class TailgateEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :tailgate_name, presence: true
+  validates :about, presence: true
+  validates :cost, presence: true
   has_many :tailgate_members, dependent: :destroy
 end
