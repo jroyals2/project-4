b92a2d119081c6861b424c456011e9bc746b09c1
jroyals2 <jamesroyals3@gmail.com>
2017-11-17 12:11:45 -0500

    added more seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,35 @@ braves = Event.create!(
     teams: "Atlanta Braves vs New York Mets"
 )
 
+Event.create!(
+    event_name: "WARRIORS GAME",
+    location: "Oakland, California",
+    zipcode: "94621",
+    date: "December 23rd, 2017",
+    teams: "Golden State Warriors vs Boston Celtics"
+)
+Event.create!(
+    event_name: "TEXAN GAME",
+    location: "Houston, Texas",
+    zipcode: "77054",
+    date: "December 31st, 2017",
+    teams: "Houston Texans vs Indianapolis Colts"
+)
+Event.create!(
+    event_name: "GIANTS GAME",
+    location: "East Rutherford, New Jersey",
+    zipcode: "07073",
+    date: "November 19th, 2017",
+    teams: "Kansas City Cheifs vs New York Giants"
+)
+Event.create!(
+    event_name: "SEAHAWKS GAME",
+    location: "Seattle, Washington",
+    zipcode: "98134",
+    date: "November 20th, 2017",
+    teams: "Atlanta Falcons vs Seattle Seahawks"
+)
+
 rise = TailgateEvent.create!(
     tailgate_name: "Rise up Birds!",
     about: "This is a great Family tailgate bring everyone!",
